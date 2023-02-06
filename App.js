93bd8a4b060dd4bad14b4121e254c7104e39f052
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2023-02-06 21:31:58 +0200

    Coloring the Components & The Overall App

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 import StartGameScreen
- from './screens/StartGameScreen';
+  from './screens/StartGameScreen';
 export default function App() {
   return (
-    <StartGameScreen />
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "#ddb52f"
+  }
+})
