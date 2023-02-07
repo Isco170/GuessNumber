13cb754760ae315903fce53c02cda304ebe47b30
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2023-02-07 22:22:09 +0200

    Switching Screens Programaticalyy

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
-import StartGameScreen
-  from './screens/StartGameScreen';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumnber) {
+    setUserNumber(pickedNumnber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+
+  if (userNumber){
+    screen = <GameScreen/>
+  }
+
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
       <ImageBackground source={require('./assets/images/background.png')} resizeMethod="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
-        <StartGameScreen />
+        {screen}
       </ImageBackground>
     </LinearGradient>
   );
@@ -18,7 +32,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#ddb52f"
   },
-  backgroundImage:{
+  backgroundImage: {
     opacity: 0.15
   }
 })
