cc6ad2cf61f6bc4acafb9df8b23c5f04d246cfb3
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2023-02-07 22:09:23 +0200

    Handling User Input & Showing an Alert Dialog

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,30 +1,47 @@
 import { useState } from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 
 function StartGameScreen() {
     const [enteredNumber, setEnteredNumber] = useState('');
 
-    function numerInputHandler(enteredText){
+    function numerInputHandler(enteredText) {
         setEnteredNumber(enteredText)
     }
 
-    function confirmInputHandler(){}
+    function resetInputHandler() {
+        setEnteredNumber('')
+    }
+
+    function confirmInputHandler() {
+        const chosenNumber = parseInt(enteredNumber)
+
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert('Invalid number',
+                'Number has to be a numer between 1 and 99.',
+                [{ text: 'Okay', style: "destructive", onPress: resetInputHandler }]
+            );
+            return;
+        }
+
+        console.log('Valid Number');
+    }
+
     return (
         <View style={styles.inputContainer}>
-            <TextInput 
-                style={styles.numberInput} 
-                maxLength={2} 
-                keyboardType="number-pad" 
-                autoCapitalize="none" 
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
                 autoCorrect={false}
                 onChangeText={numerInputHandler}
-                value={enteredNumber} 
+                value={enteredNumber}
             />
             <View style={styles.buttonsContainer}>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Reset</PrimaryButton>
+                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
                 </View>
                 <View style={styles.buttonContainer}>
                     <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
@@ -66,7 +83,7 @@ const styles = StyleSheet.create({
     buttonsContainer: {
         flexDirection: 'row'
     },
-    buttonContainer:{
+    buttonContainer: {
         flex: 1
     }
 })
